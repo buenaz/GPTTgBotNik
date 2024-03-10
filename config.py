98f4cfbc98c225bfb96eff5730aae691b617b9dc
buenaz <147922792+buenaz@users.noreply.github.com>
2024-03-10 17:42:35 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+API_URL = 'http://localhost:1234/v1/chat/completions'
+MODEL_NAME = 'mistralai/Mistral-7B-Instruct-v0.2'
+BOT_TOKEN = '7022647204:AAEbDQPnlqSQUzsNs78NqnaXgPWg97QiZzw'
+system_content = 'Ты дружелюбный ассистент, который отвечает пользователю на вопросы по разным тематикам. Ты отвечаешь кратко. Ты отвечаешь на запросы только на русском языке.'
